950f75a3857508d7b9a35156fc88e5673d3c103f
Maximilian Beck <contact@maximilianbeck.de>
2023-12-01 17:56:58 +0100

    chore: Initial setup

---
+++ day1/task1.test.ts
@@ -0,0 +1,7 @@
+import { task1 } from './task1';
+
+describe('day1/task1', () => {
+    it('should return 514579', () => {
+        expect(task1([])).toEqual(514579);
+    });
+});
---
+++ day1/task1.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const task1 = (input: string[]): number => {
+    return 1;
+};
